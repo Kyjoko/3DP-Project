799ed2a12f8d6c57869a80f190fd8ab0fcb21278
Julius Wendt <julius.wendt@live.se>
2018-09-21 14:53:04 +0200

    some basic diffuse lighting

--- 3D_Project/3D_Project/FragmentShader.glsl
+++ 3D_Project/3D_Project/FragmentShader.glsl
@@ -2,8 +2,19 @@
 
 layout(location = 0) out vec4 color;
 
+in vec4 vertexNormal;
+
 void main(){
-
-
+	
 	color = vec4(1.0, 0.0, 0.0, 1.0);
+	//color = vertexNormal;
+
+	vec4 lightDir = vec4(1.0, 1.0, 1.0, 1.0);
+	vec4 lightColor = vec4(1.0, 1.0, 1.0, 1.0);
+	float intensity = 0.2;
+
+	float d = dot(vertexNormal, lightDir);
+	d = max(d, 0);
+	color = (color + lightColor) * d * intensity;
+	color = color;
 }
--- 3D_Project/3D_Project/VertexShader.glsl
+++ 3D_Project/3D_Project/VertexShader.glsl
@@ -1,13 +1,20 @@
 #version 440
 
-layout(location = 2) uniform mat4 mat_world;
-layout(location = 3) uniform mat4 mat_view;
-layout(location = 4) uniform mat4 mat_projection;
+layout(location = 3) uniform mat4 mat_world;
+layout(location = 4) uniform mat4 mat_view;
+layout(location = 5) uniform mat4 mat_projection;
 
 layout(location = 0) in vec4 position;
+layout(location = 1) in vec2 uv;
+layout(location = 2) in vec4 normal;
+
+out vec4 vertexNormal;
+
 void main(){
 	mat4 wvp = mat_projection * mat_view * mat_world;
 
 	gl_Position = wvp * position;
+	
+	vertexNormal = normal;
 
 }
