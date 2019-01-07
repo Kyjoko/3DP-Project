163484fd87981a375d9e74f5c50e0ddd0ead1c82
Kyjoko <engstrom.samuel@gmail.com>
2019-01-07 15:05:23 +0100

    inkomplett shadowmap

--- 3D_Project/3D_Project/main.cpp
+++ 3D_Project/3D_Project/main.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_DEPRECATE
-#define GLM_FORCE_CTOR_INIT //Skillnad?
+#define GLM_FORCE_CTOR_INIT //Skillnad? <-- Noll aning
 
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
@@ -9,13 +9,13 @@
 
 #include "Display.h"
 
-const unsigned int WIDTH = 960;
-const unsigned int HEIGHT = 540;
+const unsigned int WIDTH = 1280;
+const unsigned int HEIGHT = 720;
 
 int main(void) {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	
-	Display* display = new Display(WIDTH, HEIGHT, "Hello Display");
+	Display* display = new Display(WIDTH, HEIGHT, "Best Gaem");
 	delete display;
 	
 	return 0;
